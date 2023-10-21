eef6d9bc461c6647c97afeb6e116c7a5d3e305ee
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2023-10-22 01:30:35 +0200

    vercelHostTest

--- vue-project/src/views/Home.vue
+++ vue-project/src/views/Home.vue
@@ -16,7 +16,7 @@
 
 <script>
 import io from "socket.io-client";
-const socket = io("http://localhost:3000");
+const socket = io("https://live-chatt-app-backend-dmdqyvqu0-ilyasassakalis-projects.vercel.app/");
 
 export default {
   data() {
